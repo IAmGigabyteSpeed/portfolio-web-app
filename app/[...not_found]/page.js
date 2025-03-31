cc9591f30f3e28b3471b4259952177f33cb6952b
IAmGigabyteSpeed <62113741+IAmGigabyteSpeed@users.noreply.github.com>
2025-03-31 18:09:07 +0700

    Add 404 not found page

---
+++ app/[...not_found]/page.js
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import Link from "next/link";
+import construction from "../../public/assets/construction.gif";
+
+export default function NotFound404Page() {
+  return (
+    <div className="h-screen w-screen flex flex-col items-center justify-center">
+      <Image src={construction} alt="Construction in Progress" width={300} />
+      <h1 className="text-8xl font-bold text-center">🚧404🚧</h1>
+      <p className="text-2xl text-center">
+        The page you are looking for is not here!
+      </p>
+      <p className="text-center">
+        Come back through{" "}
+        <span className="text-blue-500 hover:text-blue-900 underline">
+          <Link href="/">here</Link>
+        </span>
+      </p>
+    </div>
+  );
+}

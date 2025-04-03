6b1f17c8a6acfa86604fe2862d9bfffdb35ba922
IAmGigabyteSpeed <62113741+IAmGigabyteSpeed@users.noreply.github.com>
2025-04-03 15:16:32 +0700

    Update image size and card shadow on hover for certificate card

--- app/components/certificatecard.js
+++ app/components/certificatecard.js
@@ -1,14 +1,14 @@
 import { Card, CardContent } from "@mui/material";
 const Certificatecard = ({ company, title }) => {
   return (
-    <Card className="shadow-md hover:shadow-lg transition-shadow duration-300 rounded-xl">
+    <Card className="shadow-md hover:shadow-xl transition-shadow duration-300 rounded-xl">
       <CardContent className="rounded-xl">
         <div className="flex md:flex-row flex-col md:items-start items-center md:justify-start justify-center">
           <div>
             <img
-              className="w-full h-1/2"
+              className="w-full h-[20rem] shadow-lg mb-2"
               alt={"certificate"}
-              src={`/certificates/${title}.jpg`}
+              src={`/certificates/${title}.png`}
             />
             <div className="">
               <h2 className="text-lg text-center md:text-start font-semibold text-emerald-500">

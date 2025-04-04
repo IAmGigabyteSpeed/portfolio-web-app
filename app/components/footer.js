64035ea40cf5550f62e6c484539ba4461676db72
IAmGigabyteSpeed <62113741+IAmGigabyteSpeed@users.noreply.github.com>
2025-04-04 21:58:39 +0700

    Update footer bg  and text color

--- app/components/footer.js
+++ app/components/footer.js
@@ -10,31 +10,31 @@ const Footer = () => {
         <div className="flex flex-row md:me-4 bg-[var(--accent)] rounded-full py-2">
           <Link
             href="/"
-            className="lg:text-lg lg:px-4 md:px-2 md:mx-2 mx-1 h-full font-medium flex items-center justify-center text-center transition-all duration-300 hover:text-[var(--accent)] hover:scale-105 hover:bg-[var(--background)] rounded-xl"
+            className="lg:text-lg lg:px-4 md:px-2 md:mx-2 mx-1 h-full font-medium flex items-center justify-center text-center text-[var(--background)] transition-all duration-300 hover:text-[var(--accent)] hover:scale-105 hover:bg-[var(--background)] rounded-xl"
           >
             Home
           </Link>
           <Link
             href="/about"
-            className="lg:text-lg lg:px-4 md:px-2 md:mx-2 mx-1 h-full font-medium flex items-center justify-center text-center transition-all duration-300 hover:text-[var(--accent)] hover:scale-105 hover:bg-[var(--background)] rounded-xl"
+            className="lg:text-lg lg:px-4 md:px-2 md:mx-2 mx-1 h-full font-medium flex items-center justify-center text-center text-[var(--background)] transition-all duration-300 hover:text-[var(--accent)] hover:scale-105 hover:bg-[var(--background)] rounded-xl"
           >
             About
           </Link>
           <Link
             href="/blog"
-            className="lg:text-lg lg:px-4 md:px-2 md:mx-2 mx-1 h-full font-medium flex items-center justify-center text-center transition-all duration-300 hover:text-[var(--accent)] hover:scale-105 hover:bg-[var(--background)] rounded-xl"
+            className="lg:text-lg lg:px-4 md:px-2 md:mx-2 mx-1 h-full font-medium flex items-center justify-center text-center text-[var(--background)] transition-all duration-300 hover:text-[var(--accent)] hover:scale-105 hover:bg-[var(--background)] rounded-xl"
           >
             Blogs
           </Link>
           <Link
             href="/projects"
-            className="lg:text-lg lg:px-4 md:px-2 md:mx-2 mx-1 h-full font-medium flex items-center justify-center text-center transition-all duration-300 hover:text-[var(--accent)] hover:scale-105 hover:bg-[var(--background)] rounded-xl"
+            className="lg:text-lg lg:px-4 md:px-2 md:mx-2 mx-1 h-full font-medium flex items-center justify-center text-center text-[var(--background)] transition-all duration-300 hover:text-[var(--accent)] hover:scale-105 hover:bg-[var(--background)] rounded-xl"
           >
             Projects
           </Link>
           <Link
             href="/contact"
-            className="lg:text-lg lg:px-4 md:px-2 md:mx-2 mx-1 h-full font-medium flex items-center justify-center text-center transition-all duration-300 hover:text-[var(--accent)] hover:scale-105 hover:bg-[var(--background)] rounded-xl"
+            className="lg:text-lg lg:px-4 md:px-2 md:mx-2 mx-1 h-full font-medium flex items-center justify-center text-center text-[var(--background)] transition-all duration-300 hover:text-[var(--accent)] hover:scale-105 hover:bg-[var(--background)] rounded-xl"
           >
             Contact
           </Link>

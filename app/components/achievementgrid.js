d9a665d07f8a6d46c1bbae844de2701eb39a2637
IAmGigabyteSpeed <62113741+IAmGigabyteSpeed@users.noreply.github.com>
2025-04-04 19:06:29 +0700

    update achievement header color

--- app/components/achievementgrid.js
+++ app/components/achievementgrid.js
@@ -5,7 +5,7 @@ export default function AchievementsGrid({ name, title, organizer, link }) {
   return (
     <Card className="w-4/5 hover:bg-slate-100 hover:shadow-lg transition duration-500 p-1">
       <CardContent className="flex flex-col justify-center my-5">
-        <p className="text-xl text-emerald-500">{name}</p>
+        <p className="text-xl text-[#7D9A5A]">{name}</p>
         <p>{title}</p>
         <p className="text-gray-500">{organizer}</p>
         <Link className="text-blue-500" href={link} passHref target="_blank">
